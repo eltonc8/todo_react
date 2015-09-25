3e05fc3ac796026c5ed031db0aa19ec801174c7d
eltonc88 <eltonchan@cal.berkeley.edu>
2015-09-25 14:08:32 -0700

    setup todo model, route, gems

---
+++ db/migrate/20150925205054_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration
+  def change
+    create_table :todos, force: :cascade do |t|
+      t.string     :title, null: false
+      t.string     :body
+      t.boolean    :done,  null: false
+
+      t.timestamps         null: false
+    end
+  end
+end
