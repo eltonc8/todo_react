469625666f12044506fe1d8c7bdde1234f9b2aeb
eltonc88 <eltonchan@cal.berkeley.edu>
2015-09-25 17:21:13 -0700

    refactor todo.js, functional fetch

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -6,91 +6,70 @@
   }
   console.log("todo is evaluated");
 
-  var todo = Todo.todo = function (callback){
+  var todo = Todo.todos = function (callback){
     this.changed = callback;
     this._todos = [];
   };
 
-  todo.prototype.fetchProcessor = function (resp){
+  todo.prototype.XMLReply = function(resp, callback){
     resp = JSON.parse(resp);
-    this._todos = resp;
+    callback(resp);
     this.changed();
   };
 
-  todo.prototype.fetch = function (){
-    var url = "/api/todos/",
-        fetchProcessor = this.fetchProcessor,
-        xmlhttp = window.XMLHttpRequest && new window.XMLHttpRequest() ||
-            window.ActiveXObject && new window.ActiveXObject("Microsoft.XMLHTTP");
+  todo.prototype.XMLRequest = function(method, url, callback){
+    var fn = function (resp) { this.XMLReply(resp, callback.bind(this)); }.bind(this);
+
+    var xmlhttp = window.XMLHttpRequest && new window.XMLHttpRequest() ||
+        window.ActiveXObject && new window.ActiveXObject("Microsoft.XMLHTTP");
+    var errorMessage = "error in request, (#method), to (#url). Replied with status: ";
+    errorMessage = errorMessage.replace(/#method/, method);
+    errorMessage = errorMessage.replace(/#url/, url);
 
     xmlhttp.onreadystatechange = function (){
       if (xmlhttp.readyState == XMLHttpRequest.DONE ){
         if(xmlhttp.status == 200){
-          fetchProcessor(xmlhttp.responseText);
+          fn(xmlhttp.responseText);
         } else {
-          console.log("error in GET resquest to /api/todos/, status: " + xmlhttp.status);
+          console.log(errorMessage + xmlhttp.status);
         }
       }
     };
 
-    xmlhttp.open("GET", url, true);
+    xmlhttp.open(method, url, true);
     xmlhttp.send();
   };
 
+  todo.prototype.fetchProcessor = function (resp){
+    this._todos = resp;
+  };
+
+  todo.prototype.fetch = function (){
+    this.XMLRequest("GET", "/api/todos/", this.fetchProcessor);
+  };
+
   todo.prototype.createProcessor = function (resp){
-    resp = JSON.parse(resp);
     this._todos.push(resp);
-    this.changed();
   };
 
   todo.prototype.create = function (obj){
     var data = JSON.stringify(obj),
-        url = "/api/todos/?data=" + data,
-        createProcessor = this.createProcessor,
-        xmlhttp = window.XMLHttpRequest && new window.XMLHttpRequest() ||
-            window.ActiveXObject && new window.ActiveXObject("Microsoft.XMLHTTP");
+        url = "/api/todos/?todo=" + data;
 
-    xmlhttp.onreadystatechange = function (){
-      if (xmlhttp.readyState == XMLHttpRequest.DONE ){
-        if(xmlhttp.status == 200){
-          createProcessor(xmlhttp.responseText);
-        } else {
-          console.log("error in POST resquest to /api/todos/, status: " + xmlhttp.status);
-        }
-      }
-    };
-
-    xmlhttp.open("POST", url, true);
-    xmlhttp.send();
+    this.XMLRequest("POST", url, this.createProcessor);
   };
 
   todo.prototype.destroyProcessor = function (resp){
-    resp = JSON.parse(resp);
     var id = resp.id,
         index = this._todos.findIndex(function (obj) {return obj.id == id; });
     if (index >= 0) this._todos.splice(index, 1);
-    this.changed();
   };
 
   todo.prototype.destroy = function (obj){
     var id = obj.id,
-        url = "/api/todos/" + id,
-        deleteProcessor = this.deleteProcessor,
-        xmlhttp = window.XMLHttpRequest && new window.XMLHttpRequest() ||
-            window.ActiveXObject && new window.ActiveXObject("Microsoft.XMLHTTP");
+        url = "/api/todos/" + id;
 
-    xmlhttp.onreadystatechange = function(){
-      if (xmlhttp.readyState == XMLHttpRequest.DONE ){
-        if(xmlhttp.status == 200){
-          deleteProcessor(xmlhttp.responseText);
-        } else {
-          console.log("error in DELETE resquest to /api/todos/, status: " + xmlhttp.status);
-        }
-      }
-    };
-
-    xmlhttp.open("DELETE", url, true);
-    xmlhttp.send();
+    this.XMLRequest("DELETE", url, this.destroyProcessor);
   };
 
   todo.prototype.toggleDone = function (obj){
@@ -106,23 +85,8 @@
   todo.prototype.update = function (obj){
     var data = JSON.stringify(obj),
         id = obj.id,
-        url = "/api/todos/?data=" + data,
-        deleteProcessor = this.deleteProcessor,
-        xmlhttp = window.XMLHttpRequest && new window.XMLHttpRequest() ||
-            window.ActiveXObject && new window.ActiveXObject("Microsoft.XMLHTTP");
+        url = "/api/todos/?todo=" + data;
 
-    xmlhttp.onreadystatechange = function(){
-      if (xmlhttp.readyState == XMLHttpRequest.DONE ){
-        if(xmlhttp.status == 200){
-          deleteProcessor(xmlhttp.responseText);
-        } else {
-          console.log("error in PATCH resquest to /api/todos/, status: " + xmlhttp.status);
-        }
-      }
-    };
-
-    xmlhttp.open("DELETE", url, true);
-    xmlhttp.send();
+    this.XMLRequest("PATCH", url, this.updateProcessor);
   };
 })();
-console.log("hello");
--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,2 +1,45 @@
-class Api::TodosController < ApiController
+module Api
+  class TodosController < ApiController
+    def index
+      @todos = Todo.all
+      render json: @todos
+    end
+
+    def show
+      @todo = Todo.find(params[:id])
+      render json: @todo
+    end
+
+    def create
+      @todo = Todo.new(params[:id])
+      if @todo.save
+        render json: @todo, status: 201
+      else
+        render json: @todo.errors.full_messages, status: 422
+      end
+    end
+
+    def update
+      @todo = Todo.find(params[:id])
+      if @todo.update(todo_params)
+        render json: @todo
+      else
+        render json: @todo.errors.full_messages, status: 422
+      end
+    end
+
+    def destroy
+      @todo = Todo.find(params[:id])
+      if @todo.destroy
+        render json: @todo
+      else
+        render json: @todo.errors.full_messages, status: 422
+      end
+    end
+
+    private
+    def todo_params
+      params.permit(:title, :body, :done)
+    end
+  end
 end
