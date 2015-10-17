c97a31e75082fcd6f6b6bee16ee098e15d28b25e
eltonc88 <eltonchan@cal.berkeley.edu>
2015-10-16 23:11:29 -0700

    add routes for omniauth

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
+  before_action :set_auth
+
   def root
     render "static_pages/root"
+  end
+
+  private
+  def set_auth
+    @auth = session[:omniauth] if session[:omniauth]
   end
 end
